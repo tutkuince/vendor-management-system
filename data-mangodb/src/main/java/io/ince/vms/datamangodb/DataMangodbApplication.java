da28a07a71588d539423785d9beeabced76b046d
Tutku Ince <tutku.ince@outlook.com>
2023-11-14 21:29:07 +0300

    initial commit

---
+++ data-mangodb/src/main/java/io/ince/vms/datamangodb/DataMangodbApplication.java
@@ -0,0 +1,13 @@
+package io.ince.vms.datamangodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMangodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataMangodbApplication.class, args);
+    }
+
+}
---
+++ data-mangodb/src/test/java/io/ince/vms/datamangodb/DataMangodbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ince.vms.datamangodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMangodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
