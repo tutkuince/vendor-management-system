503dd1ddaef17bd11614fa9383b1e6702341e63f
Tutku Ince <tutku.ince@outlook.com>
2023-11-21 22:09:25 +0300

    PersonFrontEnd Project initialization

---
+++ person-frontend/src/main/java/io/ince/vms/personfrontend/PersonFrontendApplication.java
@@ -0,0 +1,13 @@
+package io.ince.vms.personfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonFrontendApplication.class, args);
+    }
+
+}
---
+++ person-frontend/src/test/java/io/ince/vms/personfrontend/PersonFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ince.vms.personfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
