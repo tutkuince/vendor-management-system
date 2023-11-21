8802ae213df7ad99e44a4644f3c34c6791c1f487
Tutku Ince <tutku.ince@outlook.com>
2023-11-21 22:15:54 +0300

    base classes have been created

--- person-frontend/src/main/java/io/ince/vms/personfrontend/controller/PersonController.java
+++ person-frontend/src/main/java/io/ince/vms/personfrontend/controller/PersonController.java
@@ -1,8 +1,13 @@
 package io.ince.vms.personfrontend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PersonController {
 
+    @GetMapping
+    public String getPerson() {
+        return "index";
+    }
 }
